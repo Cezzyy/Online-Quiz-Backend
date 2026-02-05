3d56544657c9b9b9df02618246afc95b0f459459
John Cez Casupanan <casupananjohncez@gmail.com>
2026-02-05 22:52:45 +0800

    feat(mappings): add AutoMapper configurations for question and choice DTOs

--- OnlineQuiz/Mappings/AutoMapperProfile.cs
+++ OnlineQuiz/Mappings/AutoMapperProfile.cs
@@ -95,6 +95,53 @@ namespace OnlineQuiz.Mappings
                     opt.Condition((src, dest, srcMember) =>
                         srcMember != null && !string.IsNullOrEmpty(srcMember?.ToString())));
 
+            // Question mappings
+            CreateMap<QuestionModel, QuestionDTO.QuestionDto>()
+                .ForMember(dest => dest.QuestionText, opt => opt.MapFrom(src => src.Body))
+                .ForMember(dest => dest.QuestionType, opt => opt.MapFrom(src => src.Type))
+                .ForMember(dest => dest.OrderIndex, opt => opt.MapFrom(src => src.SortOrder))
+                .ForMember(dest => dest.Choices, opt => opt.MapFrom(src => src.Choices));
+
+            CreateMap<QuestionDTO.CreateQuestionDto, QuestionModel>()
+                .ForMember(dest => dest.QuestionId, opt => opt.Ignore())
+                .ForMember(dest => dest.Body, opt => opt.MapFrom(src => src.QuestionText))
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.QuestionType))
+                .ForMember(dest => dest.SortOrder, opt => opt.MapFrom(src => src.OrderIndex))
+                .ForMember(dest => dest.Quiz, opt => opt.Ignore())
+                .ForMember(dest => dest.QuizId, opt => opt.Ignore())
+                .ForMember(dest => dest.Choices, opt => opt.Ignore())
+                .ForMember(dest => dest.AttemptAnswers, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
+
+            CreateMap<QuestionDTO.UpdateQuestionDto, QuestionModel>()
+                .ForMember(dest => dest.QuestionId, opt => opt.Ignore())
+                .ForMember(dest => dest.QuizId, opt => opt.Ignore())
+                .ForMember(dest => dest.Body, opt => opt.MapFrom(src => src.QuestionText))
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.QuestionType))
+                .ForMember(dest => dest.SortOrder, opt => opt.MapFrom(src => src.OrderIndex))
+                .ForMember(dest => dest.Quiz, opt => opt.Ignore())
+                .ForMember(dest => dest.Choices, opt => opt.Ignore())
+                .ForMember(dest => dest.AttemptAnswers, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForAllMembers(opt =>
+                    opt.Condition((src, dest, srcMember) =>
+                        srcMember != null && !string.IsNullOrEmpty(srcMember?.ToString())));
+
+            // Choice mappings
+            CreateMap<ChoiceModel, QuestionDTO.ChoiceDto>()
+                .ForMember(dest => dest.ChoiceText, opt => opt.MapFrom(src => src.Body))
+                .ForMember(dest => dest.OrderIndex, opt => opt.MapFrom(src => src.OrderIndex));
+
+            CreateMap<QuestionDTO.CreateChoiceDto, ChoiceModel>()
+                .ForMember(dest => dest.ChoiceId, opt => opt.Ignore())
+                .ForMember(dest => dest.Body, opt => opt.MapFrom(src => src.ChoiceText))
+                .ForMember(dest => dest.QuestionId, opt => opt.Ignore())
+                .ForMember(dest => dest.Question, opt => opt.Ignore())
+                .ForMember(dest => dest.AttemptAnswers, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
+
             // Role mappings
             CreateMap<RoleModel, string>().ConvertUsing(src => src.Name);
         }
